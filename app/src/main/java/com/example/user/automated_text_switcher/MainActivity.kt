8ae8d84c4904f425aade6f22ea902f82aa02a79d
AndroidDeveloperLB <lblb636@gmail.com>
2019-01-30 15:50:49 +0200

    updated stuff

--- app/src/main/java/com/example/user/automated_text_switcher/MainActivity.kt
+++ app/src/main/java/com/example/user/automated_text_switcher/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.user.automated_text_switcher
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.lb.automated_text_switcher.library.AutomatedTextSwitcher
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,5 +24,26 @@ class MainActivity : AppCompatActivity() {
         multipleTextButton.setOnClickListener {
             automatedTextSwitcher.setTextsToShow(true, "multi 1", "multi 2")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var url: String? = null
+        when (item.itemId) {
+            R.id.menuItem_all_my_apps -> url = "https://play.google.com/store/apps/developer?id=AndroidDeveloperLB"
+            R.id.menuItem_all_my_repositories -> url = "https://github.com/AndroidDeveloperLB"
+            R.id.menuItem_current_repository_website -> url = "https://github.com/AndroidDeveloperLB/AutomatedTextSwitcher"
+        }
+        if (url == null)
+            return true
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+        startActivity(intent)
+        return true
     }
 }
